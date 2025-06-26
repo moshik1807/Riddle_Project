ab117939e92626bea24fd8f99be92c26915bfcb6
moshik <moshik0527669169@gmail.com>
2025-06-26 14:58:18 +0300

    add imports file

---
+++ imports.js
@@ -0,0 +1,8 @@
+import r1 from './riddels/riddel1.js'
+import r2 from './riddels/riddel2.js'
+import r3 from './riddels/riddel3.js'
+import Player from './PlayerClass.js'
+import Riddel from './RiddleClass.js'
+export const ridd = [r1,r2,r3]
+export {Riddel}
+export {Player}
